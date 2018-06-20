3e62be2f76f0a86989db4268906e880568be735c
Pavel Kuzmin <mpakunderscore@gmail.com>
2018-06-21 02:07:45 +0300

    Fix

--- index.js
+++ index.js
@@ -12,15 +12,14 @@ server.listen(port);
 
 let spawn = require('child_process').spawn;
 
-
-let player = spawn('sh', ['player.sh', folder],{});
+let player = spawn('sh', ['player.sh', folder], {detached: true});
 
 app.get('/playlist1', function (request, response) {
 
     console.log('/playlist1')
 
-    player.kill('SIGINT');
-    player = spawn('sh', ['player.sh', '/playlist1'],{});
+    process.kill(-player.pid);
+    player = spawn('sh', ['player.sh', '/playlist1'], {detached: true});
 
     response.json({status: '/playlist1'});
 });
@@ -29,8 +28,8 @@ app.get('/playlist2', function (request, response) {
 
     console.log('/playlist2')
 
-    player.kill('SIGINT');
-    player = spawn('sh', ['player.sh', '/playlist2'],{});
+    process.kill(-player.pid);
+    player = spawn('sh', ['player.sh', '/playlist2'], {detached: true});
 
     response.json({status: '/playlist2'});
 });
