9b2e1f9daa409fd1963c57d0c7708c70f4aa7ccb
Pavel Kuzmin <mpakunderscore@gmail.com>
2018-06-21 06:45:57 +0300

    Fix

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const port = 8080;
-let folder = '/playlist2';
 
 let express = require('express');
 let app = express();
+
+//Allow cross-origin
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //STATIC WEB
 app.use('/', express.static(__dirname + '/web'));
@@ -10,18 +16,35 @@ app.use('/', express.static(__dirname + '/web'));
 let server = require('http').Server(app);
 server.listen(port);
 
+console.log('http://localhost:' + port);
+
 let spawn = require('child_process').spawn;
 
-let player = spawn('sh', ['player.sh', folder], {detached: true});
+// let player = spawn('sh', ['player.sh', folder], {detached: true});
 
 app.get('/playlist', function (request, response) {
 
     let folder = request.query.folder;
-
-    console.log(folder)
+    // console.log(folder)
 
     process.kill(-player.pid);
     player = spawn('sh', ['player.sh', folder], {detached: true});
 
     response.json({playlist: folder});
+});
+
+app.get('/list', function (request, response) {
+
+    const { readdirSync, statSync } = require('fs');
+    const { join } = require('path');
+    const dirs = p => readdirSync(p).filter(f => statSync(join(p, f)).isDirectory());
+
+    response.json({videos: dirs('videos')});
+});
+
+app.get('/scan', function (request, response) {
+
+    for (let i = 0; i < 256; i++) {
+
+    }
 });
--- web/playlist.js
+++ web/playlist.js
@@ -3,25 +3,38 @@ let protocol = 'http://';
 
 let tv = [];
 
-tv[0] = '';
-tv[1] = '192.168.0.145';
-// tv[2] = '192.168.0.164';
-// tv[3] = '192.168.0.164';
-// tv[4] = '192.168.0.164';
+tv.push('localhost');
+tv.push('192.168.0.145');
+tv.push('192.168.0.164');
+tv.push('192.168.0.180');
+tv.push('192.168.0.191');
 
-function jsonp(url, callback) {
+// console.log(tv)
 
-    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+// function jsonp(url, callback) {
+//
+//     let callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+//
+//     window[callbackName] = function (data) {
+//         console.log(data)
+//         // console.log(JSON.parse(data))
+//         delete window[callbackName];
+//         document.body.removeChild(script);
+//         callback(data);
+//     };
+//
+//     let script = document.createElement('script');
+//     script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+//     document.body.appendChild(script);
+// }
 
-    window[callbackName] = function(data) {
-        delete window[callbackName];
-        document.body.removeChild(script);
-        callback(data);
-    };
+let request = function (url) {
 
-    var script = document.createElement('script');
-    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
-    document.body.appendChild(script);
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+    xhttp.send();
+
+    return JSON.parse(xhttp.responseText);
 }
 
 let changePlaylist = function (i, playlistId) {
@@ -29,5 +42,31 @@ let changePlaylist = function (i, playlistId) {
     let url = protocol + tv[i] + port + '/playlist?folder=/playlist' + playlistId;
     // let url = 'http://localhost:8080/requests/status.xml?command=in_play&input=' + playlistName;
 
-    jsonp(url, function () {});
+    request(url);
 };
+
+let lists = function () {
+
+    for (let i = 0; i < tv.length; i++) {
+
+        let url = protocol + tv[i] + port + '/list';
+        // let url = 'http://localhost:8080/requests/status.xml?command=in_play&input=' + playlistName;
+        console.log(url);
+
+        let data = request(url);
+        console.log(data);
+
+        let div = document.createElement('div');
+        div.innerHTML += '<b>TV 2 </b>' + tv[i];
+
+        for (let j = 0; j < data.videos.length; j++) {
+
+            div.innerHTML += '<div onclick="changePlaylist(' + i + ', 1)">' + data.videos[j] + '</div>';
+        }
+
+        document.getElementById('controls').appendChild(div);
+        console.log('done')
+    }
+};
+
+lists();
